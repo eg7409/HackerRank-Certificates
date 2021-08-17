a471cc16977437bdae7d3fbcbdc8b67277f5969b
Eishita Gupta <66865791+eg7409@users.noreply.github.com>
2021-08-17 14:16:58 +0530

    Create Average Function.py

---
+++ Solutions/Python (Basic)/Average Function.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+# write your code here
+
+def avg(*n):
+    return sum(n)/len(n)
+    
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    
+    nums = list(map(int, input().split()))
+    res = avg(*nums)
+    
+    fptr.write('%.2f' % res + '\n')
+
+    fptr.close()
